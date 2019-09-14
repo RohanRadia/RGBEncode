a90d475d88583754d43de9fd5cc0e26d3282637e
RohanRadia <rohan.radia@icloud.com>
2019-09-14 22:10:43 +0100

    ascii->rgb function completed with multiple frame saves

--- encoder.py
+++ encoder.py
@@ -1,6 +1,17 @@
+from PIL import Image
+
+
+def file_to_text(file_path):
+    with open(file_path, 'r') as file:
+        data = file.read().replace('\n', '')
+
+    return data
+
+
 class Encoder:
     def __init__(self, text: str):
         self.text = text
+        self.frames = []
 
     def text_to_ascii(self):
         RGB = []
@@ -26,3 +37,38 @@ class Encoder:
             RGB[-1].append(0)
 
         return RGB
+
+    def ascii_to_rgb(self, size: int, ascii_list: list):
+        a = 0
+        b = 0
+        count = 0
+
+        frame = Image.new('RGB', (size, size), "black")
+        pixels = frame.load()
+
+        for letter in ascii_list:
+            if a is size-1 and b is size:
+                frame.save(f'frame{count}.png')
+                self.frames.append(f'frame{count}.png')
+                count += 1
+                frame = Image.new('RGB', (size, size), "black")
+                pixels = frame.load()
+                a = 0
+                b = 0
+
+            if b >= size:
+                a += 1
+                b = 0
+
+            pixels[a, b] = tuple(letter)
+            b += 1
+
+        frame.show()
+        frame.save(f'frame{count}.png')
+        self.frames.append(f'frame{count}.png')
+
+
+a = Encoder(file_to_text('Latin-Lipsum.txt'))
+ascii_holder = a.text_to_ascii()
+
+a.ascii_to_rgb(30, ascii_holder)
