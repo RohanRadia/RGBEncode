b87755bd773642383d5c2f143268c2b2189caa6b
RohanRadia <rohan.radia@icloud.com>
2020-02-20 18:47:23 +0000

    Basic structure and outline for decode made

--- RGBEncode.py
+++ RGBEncode.py
@@ -3,6 +3,6 @@ from Utils.Encode import *
 
 
 class RGBEncode:  # generate class
-    def __init__(self):  # constructor setup - runs basic code when class is instantiated
+    def __init__(self, raw_data: str, encryption_key: str = " "):  # constructor setup - runs basic code when class is instantiated
         self.decode = Decode()  # initiate Decode class
-        self.encode = Encode()  # initiate Encode class
+        self.encode = Encode(raw_data, encryption_key)  # initiate Encode class
--- Utils/Decode.py
+++ Utils/Decode.py
@@ -1,12 +1,44 @@
-from Utils.Misc import from_ascii, from_three, decrypt
+import cv2
+
+from Misc import from_ascii, from_three, decrypt
 
 
 class Decode:  # generate class
-    def __init__(self):  # constructor setup - runs basic code when class is instantiated
-        pass
+    def __init__(self, video_name: str, encryption_key: str = " "):  # constructor setup - runs basic code when
+        # class is instantiated
+        self.video_name = video_name
+        self.key = encryption_key  # class variable defined and assigned
+
+        self.raw_data = None  # class variable defined and assigned
+        self.frames = []  # class variable defined and assigned
+
+    def from_video(self):  # define required arguments
+        video = cv2.VideoCapture(f"{self.video_name}.mp4")
+
+        success, image = video.read()
+
+        count = 0
+        while success:
+            self.frames.append(f"frame{count}.jpg")
+            cv2.imwrite(f"frame{count}.jpg", image)
+            success, image = video.read()
+            count += 1
 
     def from_frames(self):  # define required arguments
         pass
 
-    def from_video(self):  # define required arguments
-        pass
+    def run(self):
+        """Deconstruct Video To Frames"""
+
+        """Deconstruct Frames To Ascii"""
+
+        """Ascii To Letters"""
+
+        """Save Text To Local .txt File"""
+
+        """Delete Frames"""
+
+
+a = Decode("encoded", "noting")
+a.from_video()
+print(a.frames)
