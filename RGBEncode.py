15522d4f34a40a41e15362ab5e9acd095c0d7ae9
RohanRadia <rohan.radia@icloud.com>
2020-02-25 20:54:03 +0000

    Added new file_name parameter to class

--- RGBEncode.py
+++ RGBEncode.py
@@ -3,6 +3,12 @@ from Utils.Encode import *
 
 
 class RGBEncode:  # generate class
-    def __init__(self, raw_data: str, encryption_key: str = " "):  # constructor setup - runs basic code when class is instantiated
-        self.decode = Decode()  # initiate Decode class
-        self.encode = Encode(raw_data, encryption_key)  # initiate Encode class
+    def __init__(self, raw_data: str = None,
+                 file_name: str = None,
+                 encryption_key: str = " "):  # constructor setup - runs basic code when class is instantiated
+        self.raw_data = raw_data
+        self.file_name = file_name
+        self.encryption_key = encryption_key
+
+#        self.decode = Decode()  # initiate Decode class
+#        self.encode = Encode(raw_data, encryption_key)  # initiate Encode class
