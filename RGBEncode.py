fbd1076cfc553c12a12ac994ee46857ce35d30a8
RohanRadia <rohan.radia@icloud.com>
2020-03-16 17:27:23 +0000

    RGBEncode class (decision making class) completed

--- RGBEncode.py
+++ RGBEncode.py
@@ -1,14 +1,34 @@
-from Utils.Decode import *
-from Utils.Encode import *
+from Utils.Decode import Decode
+from Utils.Encode import Encode
 
 
 class RGBEncode:  # generate class
-    def __init__(self, raw_data: str = None,
+    def __init__(self,
+                 raw_data: str = None,
                  file_name: str = None,
-                 encryption_key: str = " "):  # constructor setup - runs basic code when class is instantiated
+                 file_type: str = None,
+                 encryption_key: str = " ",
+                 output_frame_name: str = 'frame'):
+        # constructor setup - runs basic code when class is instantiated
         self.raw_data = raw_data
         self.file_name = file_name
+        self.file_type = file_type
         self.encryption_key = encryption_key
+        self.output_frame_name = output_frame_name
 
-#        self.decode = Decode()  # initiate Decode class
-#        self.encode = Encode(raw_data, encryption_key)  # initiate Encode class
+    def check(self):
+        process = None
+
+        if self.file_type == "png":
+            process = Decode(self.file_name, self.encryption_key)
+        elif self.file_name is not None and self.file_name != "Enter what you would like the encoded frame to be named...":
+            with open(self.file_name, 'r') as f:
+                self.raw_data = f.read()
+
+        if self.file_type != "png":
+            process = Encode(self.raw_data, self.encryption_key, self.output_frame_name)
+        return process
+
+    def call_run(self):
+        process = self.check()
+        process.run()
